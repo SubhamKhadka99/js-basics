3875d0071cc5f5163b2c9ce2aa5c5cc3638381fe
Subham Khadka <subhamkhadka99@gmail.com>
2026-01-14 07:56:08 +0545

    Promise

--- promise.js
+++ promise.js
@@ -1,142 +1,123 @@
 //! promise
-// An object that represents the eventual completion or failure of async task
-
+// object represents the eventual completion or failure of a async task.
 
 //* states of promise
+// pending
+// fulfilled
+// rejected
 
-//pending
-//fullfilled
-//rejected
+// creating a promise
 
+console.log("start");
 
-//creating promise
-// console.log('start')
+// const genPromise = () =>{
+//     return new Promise((resolve, reject) => {
+//   let rejected = false;
 
-// const promise = new Promise((resolve,reject)=>{
-    
-//     setTimeout(()=>{
-//         let rejected = true
-//         if(rejected){
-//             reject({message :'Promise error'})
-//         }
-//         else{
-//         // console.log('Promise')
-//         resolve({message:'Promise Resolved'})
+//   setTimeout(() => {
+//     // console.log('promise')
+//     if (rejected) {
+//       reject({ message: "Promise error" });
+//     } else {
+//       resolve({ message: "Promise resolved" });
 //     }
-//     },2000)
+//   }, 2000);
+// });
+// }
 
-// })
-// console.log(promise)
+// const promise = genPromise()
+
+// let loading
 
 //! promise handling
-//then runs only in success case fullfilled case
-// promise.then((data)=>{
-//     console.log(data)
-// })
-// // catch takes a callback and runs while promise is rejected
-// .catch((error)=>{
-//     console.log('Error')
-//     console.log(error)
-// })
-// //both cases then or catch
-// .finally(()=>{
-// console.log('finally')
-// })
-// console.log('end')
+// promise
+//   .then((data) => {
+//     //
+//     console.log("then");
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log("catch");
+//     console.log(error);
+//   }).finally(()=>{
+//     console.log('finally')
+//   })
 
-
-//
-
-const register = ()=>{
-    return new Promise((resolve,reject)=>{
-        let error = false;
-        setTimeout(()=>{
-            if (error){
-                reject({message : 'User register error'})
-            }
-            else{
-            resolve({message:'Register success',email:'abc@gmail.com' , password : '123456'})
-        }},3000)
-    })
-}
-
-const login = (data)=>{
-    return new Promise((resolve,reject)=>{
-        let error = false;
-        setTimeout(()=>{
-            console.log(data)
-            if (error){
-                reject({message:'User login error'})
-            }
-            else{
-                resolve({message:'Login success' , id : '12345'})
-            }
-        })
-
-
-
-    },2000)
-}
+const register = () => {
+  return new Promise((resolve, reject) => {
+    let error = false;
+    setTimeout(() => {
+      if (error) {
+        reject({ message: "User register error" });
+      } else {
+        resolve({
+          message: "register success",
+          email: "abc@gmail.com",
+          password: "123456",
+        });
+      }
+    }, 3000);
+  });
+};
 
 // const promise = register();
 
-// register()
-// .then((data)=>{
-//     console.log(data)
-// })
-// .catch((error)=>{
-//     console.log(error)
-// })
+const loginUser = (data) => {
+  return new Promise((resolve, reject) => {
+    let error = false;
+    setTimeout(() => {
+      console.log(data);
+      if (error) {
+        reject({ message: "user login error" });
+      } else {
+        resolve({ message: "login success", _id: "123" });
+      }
+    }, 2000);
+  });
+};
 
-// login()
-// .then((msg)=>{
-//     console.log(msg)
-// })
-// .catch((error)=>{
-//     console.log(error)
-// })
+// register()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// loginUser()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 //! promise chaining
-
 // register()
-// .then((data)=>{
-//     console.log(data)
-//     login(data)
-// .then((msg)=>{
-//     console.log(msg)
-// })
-// .catch((error)=>{
-//     console.log(error)
-// })
-// })
-// .catch((error)=>{
-//     console.log(error)
-// })
+//   .then((data) => {
+//     console.log(data);
+//     return loginUser(data);
+//   })
+//   .then((logindata) => {
+//     console.log(logindata);
+//     // return getprofile()
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
-//* Better practice with same output and less code
-
-register()
-.then((data)=>{
-    console.log(data)
-  return  login(data)
-}).then((data)=>{
-    console.log(data)
-})
-.catch((error)=>{
-    console.log(error)
-})
-
-//! fetch() API takes promise
-//! json() API takes promise
-
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((res)=>{
-    return res.json()
-}).then((data)=>{
-    console.log(data)
-}).catch((error)=>{
-    console.log(error)
-}).finally(()=>{
+//! fetch()
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  }).finally(()=>{
     console.log('finally')
-})
+  });
 
+console.log("end");
