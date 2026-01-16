e988b77611c063438f7939395824fbab0d9f77aa
Subham Khadka <subhamkhadka99@gmail.com>
2026-01-16 16:28:47 +0545

    functions & types

--- typescript/index.js
+++ typescript/index.js
@@ -33,7 +33,7 @@ var h;
 var arr = ['a', '1'];
 var numbers = [1, 2, 3, 4, 5]; //generic type
 //! tuples
-var user = ['ram', 20, 'ram@gmail.com'];
+// let user:[string,number,string] = ['ram',20,'ram@gmail.com']
 //! enum -> relative constant
 //! numbered enum
 // enum Role {
@@ -52,3 +52,60 @@ console.log(Role.GUEST);
 console.log(Role.USER);
 var array = 'Hello';
 console.log(array);
+//! object
+// let user:{
+//     name:string;
+//     email:string;
+//     password : string
+// } = {
+//     name : 'ram',
+//     email : 'abc@gmail.com' ,
+//     password : '123456'
+// }
+var obj;
+var user = {
+    id: '12',
+    name: 'subham',
+    email: 'subham@gmail.com',
+    password: '12345',
+    role: Role.USER,
+    phone: '12345678'
+};
+var user_One = {
+    name: 'subham',
+    email: 'subham@gmail.com',
+    password: '12345',
+    role: Role.USER,
+};
+var obj1 = {
+    id: '123',
+    name: 'as',
+    email: 'asds@gmail.com'
+};
+//! function type
+//parameter 
+var greet = function (name) {
+    console.log("Hello ".concat(name));
+};
+greet('Ram');
+//greet(12)
+//! return type
+var getFullName = function (fname, lname) {
+    return ("".concat(fname, " ").concat(lname));
+};
+//! void
+// function doesn't return anything
+var greet1 = function (name) {
+    console.log("Hello ".concat(name));
+};
+greet1('Ram');
+//! never
+// doesn't return anything 
+var func = function () {
+    throw 'error';
+};
+//add 
+var add = function (A, B) {
+    return A + B;
+};
+add(10, 20);
--- typescript/index.ts
+++ typescript/index.ts
@@ -48,7 +48,7 @@ let arr:string[] =['a','1']
 let numbers:Array<number> =[1,2,3,4,5] //generic type
 
 //! tuples
-let user:[string,number,string] = ['ram',20,'ram@gmail.com']
+// let user:[string,number,string] = ['ram',20,'ram@gmail.com']
 
 //! enum -> relative constant
 
@@ -72,3 +72,223 @@ console.log(Role.USER)
 
 let array : string = 'Hello'
 console.log(array)
+
+//! object
+
+// let user:{
+//     name:string;
+//     email:string;
+//     password : string
+// } = {
+//     name : 'ram',
+//     email : 'abc@gmail.com' ,
+//     password : '123456'
+// }
+
+let obj;
+
+
+
+//! interface
+// only defines structure
+interface IUser {
+    readonly id?:string
+    name :string;
+    email : string;
+    password : string;
+    role : Role
+    phone?:string // use ? to make optional
+}
+
+let user : IUser={
+id:'12',
+name : 'subham',
+email:'subham@gmail.com',
+password:'12345',
+role :Role.USER ,
+phone:'12345678'
+}
+
+let user_One : IUser={
+
+name : 'subham',
+email:'subham@gmail.com',
+password:'12345',
+role :Role.USER ,
+}
+
+// user.id='12'
+
+
+//! type alias
+
+type UserType = {
+    id:string,
+    name : string ,
+    email : string
+
+
+}
+
+let obj1 : UserType = {
+    id:'123',
+    name:'as',
+    email:'asds@gmail.com'
+}
+
+//! function type
+//parameter 
+
+const greet = (name:string)=>{
+    console.log(`Hello ${name}`)
+}
+greet('Ram')
+
+//greet(12)
+
+//! return type
+const getFullName = (fname:string ,lname :string):string =>{
+return(`${fname} ${lname}`)
+}
+
+//! void
+// function doesn't return anything
+const greet1 = (name:string):void=>{
+    console.log(`Hello ${name}`)
+}
+greet1('Ram')
+
+
+//typecasting
+const greet2 = (user:IUser={name :'abc'} as IUser):void=>{
+    console.log(`Hello ${user.name}`)
+}
+greet1('Ram')
+
+
+//! never
+// doesn't return anything 
+const func = ():never=>{
+    throw 'error'
+}
+
+
+//add 
+
+const add = (A:number,B:number):number =>{
+    return A+B;
+}
+
+//! type literal
+//union & intersection
+
+//defining new type 
+type STATUS = 'success' | 'error' | 'fail'
+
+//union
+let id:string | number 
+id='1'
+id=12
+
+//only takes the value assigned to new type STATUS
+let res_status : STATUS;
+res_status = "error"
+res_status="fail"
+res_status="success"
+
+// intersection 
+
+type A = {
+    a : string 
+    f : string;
+}
+
+type B = {
+    b : string 
+}
+
+type C = A & B
+
+// let D : C = {
+//  a: '',
+//  b:''
+// }
+
+//OR 
+let E : A | B ={
+    a:'',
+    b:''
+}
+
+
+//! class
+
+class user11 {
+    public name:string ;
+    protected email : string  ;
+    private password :string ;
+    id : number
+
+    constructor(name:string,email:string,password:string , id:number){
+        this.email=email,
+        this.name=name,
+        this.password=password,
+        this.id=id;
+    }
+
+    getPassword():string{
+        return this.password;
+    }
+
+}
+
+const ram:user11 = new user11('Ram' , 'Ram@gmail.com','12345',12)
+
+
+//! generic type 
+//reuseable type
+
+//? Generic interface
+
+interface IBox <T> {
+    message : string,
+    value : T;
+}
+
+let box:IBox<number[]> = {
+ message:'This is box',
+ value:[12,24]
+}
+
+let string_box:IBox<string>= {
+ message:'This is box',
+ value:'12'
+}
+
+let number_box:IBox<number>= {
+ message:'This is box',
+ value:13
+}
+
+
+//! array function
+
+const wrapper = <T>(value:T |T[]) :T[]=>{
+    
+    if(Array.isArray(value)){
+        return value
+    }
+    
+    return[value]
+}
+
+wrapper<string>('a')
+wrapper([120,20])
+
+//get first element
+
+const getFirstElement = <T> (value:T[]):T=>{
+    return value[0]
+}
+
+getFirstElement<number>([12,3456,754,35,3])
